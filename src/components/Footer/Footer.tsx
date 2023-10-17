909c82f3fd18c67fc35e0450da59f902488a6961
Georgi Dimitrov Dimitrov <60718032+GreoN5@users.noreply.github.com>
2023-10-17 20:19:51 +0300

    feat: add content from eCms from eValue

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,14 +1,36 @@
-import React, { FC } from 'react';
+'use client';
+
+import React, { FC, useEffect, useState } from 'react';
 import Link from 'next/link';
 import { BsGithub, BsInstagram, BsLinkedin } from 'react-icons/bs';
 
 const Footer: FC = () => {
+  const [copyright, setCopyright] = useState<any>();
+
+  useEffect(() => {
+    const dataCopyright = (): any => {
+      fetch(
+        `https://cms-evalue-test-ac976666bf1a.herokuapp.com/api/all-content/652ebc173e84d2c547c36bf6`,
+      )
+        .then((res) => res.json())
+        .then((data) => {
+          setCopyright(data);
+        })
+        .catch((err) => console.log(err));
+    };
+
+    dataCopyright();
+  }, []);
+
   return (
     <div className="container mx-auto w-3/4 pt-10">
       <div className="border border-zinc-300 rounded" />
       <div className="flex justify-between items-center py-7">
         <div>
-          <p className="text-slate-500">© 2023 Georgi Dimitrov</p>
+          <p
+            className="text-slate-500"
+            dangerouslySetInnerHTML={{ __html: copyright?.Copyright }}
+          ></p>
         </div>
         <div className="flex items-center">
           <Link href="/">
