366be630041b864dc0d24a03608148f557064fac
Georgi Dimitrov Dimitrov <60718032+GreoN5@users.noreply.github.com>
2023-10-17 20:37:10 +0300

    feat: add contact form from eCms

---
+++ src/app/contact/page.tsx
@@ -0,0 +1,36 @@
+import React, { FC } from 'react';
+
+const ContactPage: FC = () => {
+  return (
+    <div className="bg-white px-20 py-16 mt-5 h-full rounded-md">
+      <form
+        action="https://cms-evalue-test-ac976666bf1a.herokuapp.com/api/email-sender/652ebc173e84d2c547c36bf6"
+        method="POST"
+        className="flex flex-col"
+      >
+        <h1>Send email</h1>
+        <div className="my-3 flex flex-col">
+          <label htmlFor="sender">Sender</label>
+          <input name="sender" type="email" required className="outline" />
+        </div>
+        <div className="my-3 flex flex-col">
+          <label htmlFor="subject">Subject</label>
+          <input name="subject" type="text" required className="outline" />
+        </div>
+        <div className="my-3 flex w-full">
+          <textarea
+            className="outline w-full"
+            name="body"
+            placeholder="Message"
+            id="userMessage"
+            required
+          ></textarea>
+        </div>
+        <input type="hidden" name="origin" value="https://personal-blog-alpha-eight.vercel.app/" />
+        <input type="submit" value="Submit" />
+      </form>
+    </div>
+  );
+};
+
+export default ContactPage;
